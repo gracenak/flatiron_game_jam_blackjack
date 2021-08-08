70ca1b3b42cf5cc6db9e6b8d0e7f18fbb4aa3770
Sean <Sransonette@gmail.com>
2021-08-07 19:36:43 -0500

    deleted css in DealersCards and add fetchStand for fetch to backend

--- src/components/DealerCards.js
+++ src/components/DealerCards.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import '../DealerCards.css'
+import '../DealerCardsMap.css'
 
 const DealerCards = (props) => {
+    debugger
     return (
         <div className="DealerCardsMap">
-            {props.cards.cards.map(dcards =>
+            {props.dcards.cards.map(dcards =>
                 <div className="DealerCards">
                     <ul key="{dcards.code}"> {dcards.value} of {dcards.suit}</ul>
                     <img src={dcards.image} />
