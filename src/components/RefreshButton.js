0b0233baa809b8d1fb0aeccef7e7cc7ce2309273
JakeKViti <thevitij64@gmail.com>
2021-08-10 11:37:16 -0400

    Create RefreshButton.js

---
+++ src/components/RefreshButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class RefreshButton extends React.Component{
+
+    render() {
+    return (
+        <button>Refresh</button>
+    )};
+}
+
+export default RefreshButton;
