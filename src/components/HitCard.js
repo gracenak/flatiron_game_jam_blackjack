1f693ca5cbd663c4f0a8c844403d721f8eb3d0a0
JakeKViti <thevitij64@gmail.com>
2021-08-12 22:24:51 -0400

    Multiple Hit Cards Show Up

--- src/components/HitCard.js
+++ src/components/HitCard.js
@@ -2,28 +2,20 @@ import React from 'react';
 import '../Cards.css'
 
 
-class HitCard extends React.Component {
-
-    renderCard() {
-        for (let i = 0; i < this.props.cards.length; i++){
-        return (
-            <div className="CardsMap">
-                {this.props.cards[i].cards.map(cards =>
-                    <div className="Cards">
+const HitCard = (props) => {
+    debugger
+    return (
+        <div className="CardsMap">
+            {props.cards.map(cards =>
+                <div className="Cards">
+                    {cards.cards.map(cards =>
                         <ul key={cards.code}> {cards.value} of {cards.suit}</ul>
-                    <img src={cards.image} alt={cards.code} />
-                    </div>)}
-             </div>
-        )}
-    }
+                        
+                        )}
+                </div>)}
+         </div>
 
-    render(){
-        return (
-            <div>
-                {this.renderCard()}
-            </div>
-        )
-    }
+    )
 }
 
 export default HitCard;
