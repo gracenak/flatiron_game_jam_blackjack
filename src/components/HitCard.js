5a639e0dcde73ca34c158e3053233c1ef88c10fe
JakeKViti <thevitij64@gmail.com>
2021-08-12 21:10:41 -0400

    Create renderCard for HitCard.js

--- src/components/HitCard.js
+++ src/components/HitCard.js
@@ -3,19 +3,27 @@ import '../Cards.css'
 
 
 class HitCard extends React.Component {
-    //{for(let i=0; i < props.cards.length; i++){
 
-    //}}
+    renderCard() {
+        for (let i = 0; i < this.props.cards.length; i++){
+        return (
+            <div className="CardsMap">
+                {this.props.cards[i].cards.map(cards =>
+                    <div className="Cards">
+                        <ul key={cards.code}> {cards.value} of {cards.suit}</ul>
+                    <img src={cards.image} alt={cards.code} />
+                    </div>)}
+             </div>
+        )}
+    }
+
     render(){
-    return (
-        <div className="CardsMap">
-            {this.props.cards[0].cards.map(cards =>
-                <div className="Cards">
-                    <ul key={cards.code}> {cards.value} of {cards.suit}</ul>
-                <img src={cards.image} alt={cards.code} />
-                </div>)}
-         </div>
-    )}
+        return (
+            <div>
+                {this.renderCard()}
+            </div>
+        )
+    }
 }
 
 export default HitCard;
