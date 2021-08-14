8d0a57814eaf3db50d0eebcacb1477be46025aa4
JakeKViti <thevitij64@gmail.com>
2021-08-13 20:30:01 -0400

    Convert CardsArray to Class Component, Add HandleClick

--- src/components/cardsArray.js
+++ src/components/cardsArray.js
@@ -2,17 +2,24 @@ import React from 'react';
 import '../Cards.css'
 
 
-const CardsArray = (props) => {
-    return (
+class CardsArray extends React.Component {
+    handleClick(){
+
+    }
+
+
+    render(){
+        return (
         <div className="CardsMap">
-            {props.cards.cards.map(cards =>
+            {this.props.cards.cards.map(cards =>
                 <div className="Cards">
                     <ul key={cards.code}> {cards.value} of {cards.suit}</ul>
                     <img src={cards.image} alt={cards.code}/>
                 </div>)}
+                <button onClick={() => this.handleClick()}>Send Card Info</button>
         </div>
-
-    )
+        )
+    }
 }
 
 export default CardsArray;
