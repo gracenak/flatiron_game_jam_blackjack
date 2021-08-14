b4b0ff014f31da4190c3d989d2029134cecdf34f
JakeKViti <thevitij64@gmail.com>
2021-08-14 13:08:19 -0400

    Convert CurrentCardTotal to class component, and Put mapDispatchToProps in there

--- src/components/CurrentCardsTotal.js
+++ src/components/CurrentCardsTotal.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import { sendCardInfo } from "../actions/sendCardInfo";
+import { connect } from 'react-redux'
 
-const CurrentCardsTotal = (props) => {
-    return (
-        <div className={"TotalHandValue"}>
-            Hand Value:
-        </div>
-    );
+class CurrentCardsTotal extends React.Component {
+
+    render(){
+        return (
+            <div className={"TotalHandValue"}>
+                Hand Value:
+            </div>
+        );
+    }
 }
 
 
 
-export default CurrentCardsTotal;
+const mapDispatchToProps = dispatch => {
+    return {
+        sendCardInfo: (entries) => {
+        dispatch(sendCardInfo(entries))
+      }
+    };
+  };
+  
+  export default connect(null, mapDispatchToProps)(CurrentCardsTotal);
