0652139fac945110835dea509d2bf08eef4beadb
JakeKViti <thevitij64@gmail.com>
2021-08-15 19:19:05 -0400

    Hit cards are added to player calcuations

--- src/components/StandButton.js
+++ src/components/StandButton.js
@@ -7,10 +7,16 @@ const StandButton = (props) => {
 
 
     let HandleClick = () => {
-        if (playerCardsTotal() > dealerCardsTotal()) {
-            alert("Player Wins")
+        alert("Player Has " + hitCardsTotal() + "!")
+        alert("Dealer Has " + dealerCardsTotal() + "!")
+        if (hitCardsTotal() > 21) {
+            alert("Player Has Busted!")
+        } else if (hitCardsTotal() > dealerCardsTotal()) {
+            alert("Player Wins!")
+        } else if (hitCardsTotal() === dealerCardsTotal()) {
+            alert("Player Pushes!")
         } else {
-            alert("Player Loses")
+            alert("Player Loses!")
         }
 
 
@@ -27,55 +33,51 @@ const StandButton = (props) => {
             if (dealerHand[i] === "KING" || dealerHand[i] === "QUEEN" || dealerHand[i] === "JACK"){
                 total.push(10)
             } else if (dealerHand[i] === "ACE"){
-                total.push(10)
+                total.push(11)
             } else {
                 total.push(parseInt(dealerHand[i], 10))
             }
         }
         let Dealertotal = total[0] + total[1] 
-        console.log(Dealertotal)
         return Dealertotal
     }
 
-    const playerCardsTotal = () => {
-
-        let playerHand = []
+     let hitCardsTotal = () => {
+        let finalTotal = []
         let total = []
+        let finalScore = 0
         props.cards.cards.map(cards =>
-            playerHand.push(cards.value)
+            finalTotal.push(cards.value)
         )
-        for (let i = 0; playerHand.length > i; i++){
-            if (playerHand[i] === "KING" || playerHand[i] === "QUEEN" || playerHand[i] === "JACK"){
+        if (props.hit.length !== 0){
+            props.hit.map(cards =>
+                {cards.cards.map(cards =>
+                    finalTotal.push(cards.value)
+                )}
+            )
+        }
+        for (let i = 0; finalTotal.length > i; i++){
+            if (finalTotal[i] === "KING" || finalTotal[i] === "QUEEN" || finalTotal[i] === "JACK"){
                 total.push(10)
-            } else if (playerHand[i] === "ACE"){
+            } else if (finalTotal[i] === "ACE"){
                 total.push(10)
             } else {
-                total.push(parseInt(playerHand[i], 10))
+                total.push(parseInt(finalTotal[i], 10))
             }
         }
-        let playerTotal = total[0] + total[1] 
-        console.log(playerTotal)
-        return playerTotal
+        
+        for (let i = 0; total.length > i; i++){
+            finalScore += total[i]
+        }
+        return finalScore
 
-    }
-
-    // let hitCardsTotal = () => {
-
-    //     let totalHand = []
-
-    //     props.hit.cards.map(hcards =>
-    //         totalHand.push(parseInt(hcards.value, 10))
-    //     )
-    //     alert(totalHand.reduce((a, b) => a + b, 0))
-
-    // }
+     }
 
     // let playerFinalTotal = playerCardsTotal() + hitCardsTotal()
 
     return (
         <div>
         <button onClick={() => HandleClick()}>Stand</button>
-        <p>{playerCardsTotal}</p>
         </div>
     )
 };
