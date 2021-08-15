065bb268c772fae152bafc2d1dad19308a78c2ba
Grace Nakano <gracenak@gmail.com>
2021-08-15 08:45:56 -0700

    add face down dealer card

--- src/components/DealerCards.js
+++ src/components/DealerCards.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import '../DealerCards.css'
+import '../DealerCards.css';
+import cardDown from '../components/images/cardDown.png';
 
 const DealerCards = (props) => {
+
+
     return (
         <div className="HitCardsMap">
-            
-            <ul key={props.dcards.code}> Dealer Has One Card Faced Down And The {props.dcards.value} of {props.dcards.suit} Faced Up <br></br> <img src={props.dcards.image} alt={props.dcards.code}/></ul>
-         </div>
+            <div className="card-inner">
+                <div className="card-back">
+                    <img src={cardDown} width="226px" height="314px" alt="card-down" /> 
+                </div>
+                <div className="card-front">
+                    <ul key={props.dcards.code}>{props.dcards.value} of {props.dcards.suit}<br></br><img src={props.dcards.image} alt={props.dcards.code}/></ul>
+                </div>
+            </div>
+        </div>
     )
 }
 
