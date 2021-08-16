bfa8d7e05ec1d7cba784839dbeba08b6a62908b4
JakeKViti <thevitij64@gmail.com>
2021-08-15 20:41:00 -0400

    Create Gif for README

--- src/components/DealerCards.js
+++ src/components/DealerCards.js
@@ -7,16 +7,8 @@ const DealerCards = (props) => {
 
     return (
         <div className="HitCardsMap">
-            <div className="card-flip">
-                <div className="card-inner">
-                    <div className="card-back">
-                        <img src={cardDown} width="226px" height="314px" alt="card-down" /> 
-                    </div>
-                    <div className="card-front">
+        
                         <ul key={props.dcards.code}>{props.dcards.value} of {props.dcards.suit}<br></br><img src={props.dcards.image} alt={props.dcards.code}/></ul>
-                    </div>
-                </div>
-            </div>
         </div>
     )
 }
