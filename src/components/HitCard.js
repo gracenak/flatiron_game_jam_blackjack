ae12c977c5d104cc282f5e9607e8fce051756c54
jakekviti <thevitij64@gmail.com>
2021-09-17 21:31:47 -0400

    Convert HitCard to a class component

--- src/components/HitCard.js
+++ src/components/HitCard.js
@@ -2,17 +2,19 @@ import React from 'react';
 import '../HitCards.css'
 
 
-const HitCard = (props) => {
-    return (
-        <div className="HitCardsMap">
-            {props.cards && props.cards.map(cards =>
-                <div className="HitCards">
-                    {cards.cards.map(cards =>
-                        <ul key={cards.code}>  {cards.value} of {cards.suit} <br></br> <img src={cards.image} alt={cards.code}/></ul>
-                    )}
-                </div>)}
-         </div>
-    )
+class HitCard extends React.Component {
+    render(){
+        return (
+            <div className="HitCardsMap">
+                {this.props.cards && this.props.cards.map(cards =>
+                    <div className="HitCards">
+                        {cards.cards.map(cards =>
+                            <ul key={cards.code}>  {cards.value} of {cards.suit} <br></br> <img src={cards.image} alt={cards.code}/></ul>
+                        )}
+                    </div>)}
+            </div>
+        )
+    }
 }
 
 export default HitCard;
